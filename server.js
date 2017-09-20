2341854e25c75e39e44b1aaa73154f47310cb0a3
asdfadefwffhb <32115231+asdfadefwffhb@users.noreply.github.com>
2017-09-20 09:00:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    app     = express();
-var request = require('request');
-    
+    request = require('request'),
+    app     = express();    
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/http/daocloud/restart/:appid/:token',function(req, res){
+app.get('/:appid/:token',function(req, res){
     var appid = req.params.appid;
     var token = req.params.token;
     request({
